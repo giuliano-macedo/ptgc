d240dedc0a16e23a5d1e4ab3af0b9ecd622c61b4
Giuliano Oliveira <giuliano.llpinokio@gmail.com>
2019-08-05 22:26:00 -0400

    i think i finished lexer

--- src/lex.py
+++ src/lex.py
@@ -1,13 +1,13 @@
-import json
 import re
 from argparse import ArgumentParser
 from itertools import chain,tee
 import itertools
 import ox
 from os import remove
+import json
 
 quote=r"(\'|\")"
-char=r"[a-zA-z]"
+char=r"[a-zA-Z]"
 integer=r"(\+|\-|)\d+"
 float_classic=rf"{integer}(\.\d*)"
 float_e=rf"({float_classic}|{integer})((e|E)({float_classic}|{integer}))?"
@@ -21,8 +21,28 @@ tokens=[
 	("END_BLOCK",r";"),
 	("MAIN_START",r"in(i|í)cio"),
 	("MAIN_END",r"fimalgor(i|í)tmo"),
+	("OPEN_P",r"\("),
+	("CLOSE_P",r"\)"),
+	("ELSE",r"sen(a|ã)o"),
+	("IF",r"se"),
+	("THEN",r"ent(a|ã)o"),
+	("IF_END",r"fimse"),
+	("FOR",r"para"),
+	("FROM",r"de"),
+	("UNTIL",r"at(e|é)"),
+	("STEP",r"passo"),
+	("DO",r"fa(c|ç)a"),
+	("FOR_END",r"fimpara"),
+	("WHILE",r"enquanto"),
+	("WHILE_END",r"fimenquanto"),
+	("SWITCH",r"escolha"),
+	("DEFAULT",r"casopadr(a|ã)o"),
+	("CASE",r"caso"),
+	("STOP",r"pare"),
+	("SWITCH_END","fimescolha"),
 	("ATTR",r"<-"),
-	("AROP",r"\+|\-|\*|\/"),
+	("AROP",r"\+|\-|\*|\/|MOD|\^"),
+	("BINOP",r"\>|\>\=|\<|\<\=|\=|\!\="),
 	("NUMBER",rf"{float_classic}|{float_e}"),
 	("ID",rf"{char}\w*"),
 ]
@@ -33,10 +53,23 @@ args=parser.parse_args()
 
 with open(args.input,encoding="utf-8") as f:
 	lexer = ox.make_lexer(tokens)
-	out=[(obj.type,obj.value) for obj in lexer(f.read())]
-print(json.dumps(out,indent=4,ensure_ascii=False))
+	_err=None
+	try:
+		out=[(obj.type,obj.value) for obj in lexer(f.read())]
+	except Exception as e:
+		_err=e
+	#lexer() create this 2 files for some reason
+	try:
+		remove("parsetab.py")
+		remove("parser.out")
+	except Exception:pass
+	if _err:
+		raise _err
 with open("tokens.json","w") as f:
-	json.dump({"tokens":out},f,indent=4,ensure_ascii=False)
-# lexer() creates this 2 files for some reason
-remove("parsetab.py")
-remove("parser.out")
+	f.write("{\"tokens\":[\n")
+	json_str=lambda s:json.dumps(s,ensure_ascii=False)
+	for token_name,token_value in out:
+		f.write(f"\t[{json_str(token_name)},{json_str(token_value)}],\n")
+	if len(out)!=0:
+		f.seek(f.tell()-2)
+	f.write("\n]}")
