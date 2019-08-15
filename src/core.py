007a5b904ad213b88385200941617964138c5e11
Giuliano Oliveira <giuliano.llpinokio@gmail.com>
2019-08-15 12:02:41 -0400

    More modularized, using transformers/visitors

---
+++ src/core.py
@@ -0,0 +1,169 @@
+from lark import Visitor,Transformer,Token
+def get_symtable(tree):
+	assert tree.data=="start"
+	visitor=SymtableVisitor()
+	vars_block=tree.children[3]
+	visitor.visit(vars_block)
+	return visitor.symtable
+def check_semantic(symtable,tree):
+	assert tree.data=="start"
+	main=tree.children[5]
+	CheckIdsVisitor(symtable).visit(main)
+	CheckExpsTransformer(symtable).transform(main)
+def ptg2py(symtable,tree):
+	return ptg2pyTransformer(symtable).transform(tree)
+class SymtableVisitor(Visitor):
+	def __init__(self,*args,**kwargs):
+		super().__init__(*args,**kwargs)
+		self.symtable={
+			"escreva":"function",
+			"leia":"function"
+		}
+	def vars_block(self,tree):
+		_type=tree.children[-1].value
+		_vars=tree.children[:-1]
+		for v in _vars:
+			if self.symtable.get(v)!=None:
+				raise SyntaxError(f"{v} is already defined")
+			self.symtable[v.value]=_type
+
+	def visit(self,tree):
+		super().visit(tree)
+class CheckIdsVisitor(Visitor):
+	def __init__(self,symtable,*args,**kwargs):
+		super().__init__(*args,**kwargs)
+		self.symtable=symtable
+	def __default__(self,tree):
+		for t in (c for c in tree.children if type(c)==Token):
+			if (t.type=="ID") and (self.symtable.get(t.value)==None):
+				raise SyntaxError(f"id {t.value} is not defined!")
+class CheckExpsTransformer(Transformer):
+	def __init__(self,symtable,*args,**kwargs):
+		super().__init__(*args,**kwargs)
+		self.symtable=symtable
+	def exp(self,args):
+		if len(args)==1:
+			t=args[0]
+			if t.type == "STRING":
+				return "caractere"
+			elif t.type=="NUMBER":
+				return "real"
+			elif t.type=="ID":
+				return self.symtable[t.value]
+			return
+		if type(args[0])==Token:#parentesis exp
+			return args[1]
+		a,op,b=args
+		if (a=="caractere") or (b=="caractere"):
+			#if both a,b are string and operation is not adition or they are different and regardless of the operation
+			if ((a==b) and op.value!="+") or (a!=b):
+				raise SyntaxError(f"Error, operation {{string ({op.value}) non-string }} is invalid")
+			return "caractere"
+		elif (a in {"inteiro","real"}) or (b in {"inteiro","real"}): #this is always true
+			if op.type not in {"BINOP","AROP"}:
+				raise SyntaxError(f"Error, operation ({op.value}) invalid")
+			return "real"
+ptgtype2pytype={
+	"caractere":"str",
+	"real":		"float",
+	"inteiro":	"int"
+}
+exp_hook={
+	"^":"**",
+	"e":"and",
+	"ou":"or",
+	"MOD":"%",
+	"=":"=="
+}
+def tabulate(string):
+	return "\n".join(["\t"+s for s in string.split("\n")[:-1]])+"\n"
+class ptg2pyTransformer(Transformer):
+	def __init__(self,symtable,*args,**kwargs):
+		super().__init__(*args,**kwargs)
+		self.symtable=symtable
+	def _buildvars(self):
+		ans=""
+		for _var,_type in {k:v for k,v in self.symtable.items() if v!="function"}.items():
+			ans+=(f"{_var}={ptgtype2pytype[_type]}()\n")
+		return ans
+	def start(self,args):
+		label=f"#{args[1][1:-1]}\n"
+		main=args[-2]
+		return label+self._buildvars()+main
+	def main(self,args):
+		return "".join(args)
+	def command(self,args):
+		return "".join(args)
+	def function_call(self,args):
+		return "".join(args)
+	def command_loop(self,args):
+		return "".join(args)
+	def command_conditional(self,args):
+		return "".join(args)
+	def if_body(self,args):
+		if len(args)==0:
+			return "pass\n"
+		return tabulate("".join(args))
+	def switch_body(self,args):
+		return self.if_body(args)
+	def for_body(self,args):
+		return self.if_body(args)
+	def while_body(self,args):
+		return self.if_body(args)
+	def attribution(self,args):
+		_id=args[0]
+		_exp=args[2]
+		return f"{_id}={_exp}\n"
+	def exp(self,args):
+		if len(args)==3 and type(args[1])==Token:
+			args[1]=exp_hook.get(args[1],args[1])
+		return " ".join(args)
+	def command_increment(self,args):
+		if type(args[-1])!=Token:#if its not token then there is not a ++ token in the end
+			return "".join(args)+"\n"
+		return f"{args[0]}+=1\n"
+	def stop(self,args):
+		return args[0]+"\n"
+	def function_print(self,args):
+		return f"print({','.join(args)})\n"
+	def function_scan(self,args):
+		_id=args[0]
+		_type=ptgtype2pytype[self.symtable[_id]]
+		return f"{_id}={_type}(input())\n"
+	def command_if(self,args):
+		ans=f"if({args[1]}):\n{args[3]}"
+		for i,t in enumerate(args[4:],4):
+			if type(t)==Token and t.type=="ELSE":
+				nt=args[i+1]
+				if type(nt)==Token and nt.type=="IF":
+					ans+=f"elif ({args[i+2]}):\n{args[i+4]}"
+				else:
+					ans+=f"else:\n{args[i+1]}"
+		return ans
+	def command_switch(self,args):
+		_id=args[1]
+		state=0
+		ans=""
+		for i,t in enumerate(args[2:],2):
+			if type(t)==Token:
+				if t.type=="CASE":
+					body=args[i+2].split("\n")
+					if body[-2].strip()!="pare":
+						raise SyntaxError(f"switch case without break not implemented")
+					del body[-2]
+					body="\n".join(body)
+					ans+=f"{['if','elif'][state]} {_id}==({args[i+1]}):\n{body}"
+					state=1
+				elif t.type=="DEFAULT":
+					ans+=f"else:\n{args[i+1]}"
+		return ans
+
+	def command_for(self,args):
+		_id=args[1]
+		a,b,c=(args[c] for c in (3,5,7))
+		body=args[-2]
+		return f"for {_id} in range({a},{b},{c}):\n{body}"
+	def command_while(self,args):
+		exp=args[1]
+		body=args[-2]
+		return f"while({exp}):\n{body}"
--- src/ptgc.py
+++ src/ptgc.py
@@ -1,157 +1,24 @@
 from lark import Lark,Token
-from graphviz import Digraph
+import graphviz
 import argparse
-def build_dot(tree,dot):
-	h=str(id(tree))
-	label=getData(tree) if istoken(tree) else f"<<font face=\"boldfontname\">{getData(tree)}</font>>"
-	dot.node(h,label=label)
-	if istoken(tree):
-		return 
-	for children in tree.children:
-		dot.edge(h,str(id(children)))
-		build_dot(children,dot)
-def check_ids(t):
-	if type(t)!=Token:
-		for c in t.children:
-			check_ids(c)
-		return
-	if t.type!="ID":
-		return
-	if symtable.get(t.value)==None:
-		raise SyntaxError(f"id {t.value} is not defined!")
-def check_exps(t):
-	if type(t)==Token:
-		if t.type == "STRING":
-			return "caractere"
-		elif t.type=="NUMBER":
-			return "real"
-		elif t.type=="ID":
-			return symtable[t.value]
-		return
-	if t.data!="exp":
-		for c in t.children:
-			check_exps(c)
-	else:
-		if len(t.children)==3:
-			a,op,b=t.children
-			atype,btype=check_exps(a),check_exps(b)
-			# print("value",a.data,op.value,b.data)
-			# print("type ",atype,op.value,btype)
-			if (atype=="caractere") or (btype=="caractere"):
-				#if both a,b are string and operation is not adition or they are different and regardless of the operation
-				if ((atype==btype) and op.value!="+") or (atype!=btype):
-					raise SyntaxError(f"Error, operation {{string ({op.value}) non-string }} is invalid")
-				return "caractere"
-			elif (atype in {"inteiro","real"}) or (btype in {"inteiro","real"}): #this is always true
-				if op.type not in {"BINOP","AROP"}:
-					raise SyntaxError(f"Error, operation ({op.value}) invalid")
-				return "real"
-		else:
-			return check_exps(t.children[0])
+from core import get_symtable,check_semantic,ptg2py	
 
+class Lark2Graphviz(graphviz.Digraph):
+	def __init__(self,tree,*args,**kwargs):
+		super().__init__(*args,**kwargs)
+		self.__build_dot(tree)
+	def __build_dot(self,tree):
+		istoken=lambda obj:type(obj)==Token
+		getData=lambda obj:obj if istoken(obj) else obj.data
+		h=str(id(tree))
+		label=getData(tree) if istoken(tree) else f"<<font face=\"boldfontname\">{getData(tree)}</font>>"
+		self.node(h,label=label)
+		if istoken(tree):
+			return 
+		for children in tree.children:
+			self.edge(h,str(id(children)))
+			self.__build_dot(children)
 
-# intreal={"inteiro","real"}
-# char={"caractere"}
-# exp_rules=[
-# 	(intreal,{"AROP","BINOP"},intreal),
-# 	(char,{"+"},char)
-# ]
-
-def command_f(sub_tree,tab=0):
-	for command in (o.children[0] for o in sub_tree.children):
-		command_hook[command.data](command,tab)
-
-def inorder(t):
-	if type(t)==Token:
-		return exp_hook.get(t.value,t.value)
-	ans=[]
-	for c in t.children:
-		ans.append(inorder(c))
-	return " ".join(ans)
-exp_hook={
-	"^":"**",
-	"e":"and",
-	"ou":"or",
-	"MOD":"%",
-	"=":"=="
-}
-def exp_eval(sub_tree):
-	return inorder(sub_tree)
-def attr_f(sub_tree,tab):
-	children=sub_tree.children
-	write(f"{children[0].value}={exp_eval(children[-1])}\n",tab)
-def inc_f(sub_tree,tab):
-	children=sub_tree.children
-	# breakpoint()
-	if type(children[-1])!=Token:#if its not token then there is not a ++ token in the end
-		write(f"{inorder(sub_tree)}\n",tab)
-	else:
-		write(f"{children[0].value}+=1\n",tab)
-def function_f(sub_tree,tab):
-	f=sub_tree.children[0]
-	if f.data=="function_print":
-		exp=[c if type(c)==Token else exp_eval(c) for c in f.children]
-		write(f"print({','.join(exp)})\n",tab)
-	else:
-		var=f.children[0]
-		write(f"{var}={ptgtype2pytype[symtable[var]]}(input())\n",tab)
-def conditionals_f(sub_tree,tab):
-	conditional=sub_tree.children[0]
-	children=conditional.children
-	if conditional.data=="command_if":
-		write(f"if ({exp_eval(children[1])}):\n",tab)
-		command_f(children[3],tab+1)
-
-		for i,c in enumerate(children[4:],4):
-			if type(c)!=Token:
-				continue
-			if c.type=="ELSE":
-				if type(children[i+1])==Token:
-					write(f"elif ({exp_eval(children[i+2])}):\n",tab)
-					command_f(children[i+4],tab+1)
-				else:
-					write("else:\n",tab)
-					command_f(children[i+1],tab+1)
-	elif conditional.data=="command_switch":
-		_id=children[1].value
-		numbers_if=0
-		for i,c in enumerate(children):
-			if type(c)!=Token:
-				continue
-			if c.type=="CASE":
-				write(f"{['if','elif'][numbers_if>0]} {_id}==({exp_eval(children[i+1])}):\n")
-				command_f(children[i+2],tab+1)
-				try:
-					assert children[i+2].children[-1].children[0].data == "stop"
-				except Exception as e:
-					breakpoint()
-					raise SyntaxError(f"switch case without break not implemented")
-					raise e
-				numbers_if+=1
-
-
-
-		
-def loops_f(sub_tree,tab):
-	conditional=sub_tree.children[0]
-	children=conditional.children
-	if conditional.data=="command_while":
-		write(f"while ({exp_eval(children[1])}):\n",tab)
-		command_f(children[3],tab+1)
-	elif conditional.data=="command_for":
-		write(f"for {children[1].value} in range({','.join([exp_eval(c) for c in children[3:8:2]] )}):\n",tab)
-		command_f(children[-2],tab+1)
-command_hook={
-	"attribution"			:attr_f,
-	"command_increment"		:inc_f,
-	"stop"					:lambda s,t:None,
-	"function_call"			:function_f,
-	"command_conditional"	:conditionals_f,
-	"command_loop"			:loops_f,
-}
-
-istoken=lambda obj:type(obj)==Token
-getData=lambda obj:obj if istoken(obj) else obj.data
 
 parser=argparse.ArgumentParser()
 parser.add_argument("input",type=argparse.FileType("r"))
@@ -161,31 +28,7 @@ args=parser.parse_args()
 with open("grammar.lark") as f:
 	lark=Lark(f.read())
 tree=lark.parse(args.input.read())
-main=tree.children[-2]
-dot = Digraph(format="pdf")
-build_dot(tree,dot)
-dot.render('tree.dot')
-def write(s,tab=0):
-	args.output.write(("\t"*tab)+s)
-write(f"#{tree.children[1].value[1:-1]}\n")
-#vars
-ptgtype2pytype={
-	"caractere":"str",
-	"real":		"float",
-	"inteiro":	"int"
-}
-symtable={"escreva":"function","leia":"function"}
-for var in tree.find_data("vars_block"):
-	_type=var.children[-1].value
-	t=ptgtype2pytype[_type]+"()"
-	_vars=var.children[:-1]
-	for v in _vars:
-		if symtable.get(v)!=None:
-			raise SyntaxError(f"{v} is already defined")
-		symtable[v]=_type
-	write(f"{','.join(_vars)}={','.join([t]*len(_vars))}\n")
-check_ids(main)
-check_exps(main)
-#----------------------------------------------------------------
-command_f(main)
-# breakpoint()
+Lark2Graphviz(tree,format="pdf").render("tree.dot")
+symtable=get_symtable(tree)
+check_semantic(symtable,tree)
+args.output.write(ptg2py(symtable,tree))
